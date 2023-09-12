14e848c2fad71aef7f098030ae4d8b45ca929d3a
Tyler Sullberg <tysullberg@gmail.com>
2023-09-12 15:58:22 -0700

    Create home page.

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,12 +1,22 @@
 import { ApolloProvider } from '@apollo/client'
+import { Global, css } from "@emotion/react";
 import { apolloClient } from './apollo'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routers/router'
 import { CurrentUserProvider } from './contexts/current_user_context'
 
+const GlobalStyles = css`
+    html, body, #root, .MuiContainer-root {
+         height: 100%;
+         margin: 0;
+         padding: 0;
+    }
+`
+
 function App() {
   return (
     <ApolloProvider client={apolloClient}>
+      <Global styles={GlobalStyles} />
       <CurrentUserProvider>
         <RouterProvider router={router} />
       </CurrentUserProvider>
--- apps/frontend/src/components/Home.tsx
+++ apps/frontend/src/components/Home.tsx
@@ -1,1 +1,53 @@
-export const Home = () => <h1>Cults</h1>;
+import styled from '@emotion/styled';
+import Button from '@mui/material/Button';
+
+import { ConnectToDiscord } from "./ConnectToDiscord";
+
+const PageContainer = styled.div`
+    display: flex;
+    padding: 0px 16px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    flex: 1 0 0;
+    align-self: stretch;
+    height: 100%;
+`
+
+const Subtitle = styled.div`
+    color: #000;
+/* M3/title/large */
+font-family: Roboto;
+font-size: 22px;
+font-style: normal;
+font-weight: 400;
+line-height: 28px; /* 127.273% */
+`
+
+const Logo = styled.div`
+    color: var(--m-3-sys-light-primary, #6750A4);
+    text-align: center;
+    font-family: Roboto;
+    font-size: 120px;
+    font-style: italic;
+    font-weight: 700;
+    line-height: 120px; /* 100% */
+`
+
+const ButtonsContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+`
+
+
+export const Home = () => <PageContainer>
+    <Logo>Cults</Logo>
+    <Subtitle>Process is king</Subtitle>
+    <ButtonsContainer>
+        <Button variant='contained' color="primary" href="/api/auth/discord/login">Join Alpha Waitlist</Button>
+        <ConnectToDiscord/>
+    </ButtonsContainer>
+    </PageContainer>;
