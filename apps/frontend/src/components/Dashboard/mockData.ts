7e287b7530507d36df2016aa68531f34f02e6060
Tyler Sullberg <tysullberg@gmail.com>
2023-09-14 21:22:22 -0700

    Create mock data for 'requests'.

---
+++ apps/frontend/src/components/Dashboard/mockData.ts
@@ -0,0 +1,35 @@
+import { RequestProps } from "./Table";
+
+export const mockData: RequestProps[] = [
+  {
+    process: "Send ETH from TEC treasury",
+    request: "Send award to TEC hackathon winner",
+    creator: [
+      {
+        name: "tsully",
+        url: "https://cdn.discordapp.com/avatars/698194276101914774/487b3c7e19c14f456d12d5aea5cf3c71.png?size=128",
+      },
+      {
+        name: "popp",
+        url: "https://cdn.discordapp.com/avatars/707707546114457641/3947a78996ba9e32703b635a40de6822.webp?size=240",
+      },
+      {
+        name: "t sully",
+        url: "",
+      },
+      {
+        name: "t sully",
+        url: "",
+      },
+    ],
+    respond: [
+      {
+        name: "tsully",
+        url: "https://cdn.discordapp.com/avatars/698194276101914774/487b3c7e19c14f456d12d5aea5cf3c71.png?size=128",
+      },
+    ],
+    expirationDate: "4 hours",
+    decisionType: "Threshold vote",
+    userVote: "string",
+  },
+];
