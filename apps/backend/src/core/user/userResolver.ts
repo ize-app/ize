5dadb15af0d6d9fc9f790f51e10213bfc722b87f
Tyler Sullberg <tysullberg@gmail.com>
2024-04-30 21:06:40 -0700

    fix avatar url resolution.

--- apps/backend/src/core/user/userResolver.ts
+++ apps/backend/src/core/user/userResolver.ts
@@ -1,10 +1,29 @@
 import { User } from "@graphql/generated/resolver-types";
 import { UserPrismaType } from "./userPrismaTypes";
+import { DiscordApi } from "@/discord/api";
 
 export const userResolver = (user: UserPrismaType): User => {
   return {
     id: user.id,
     name: user.firstName ? user.firstName + " " + user.lastName : "User",
     createdAt: user.createdAt.toString(),
+    icon: findFirstIdentityIcon(user),
   };
+};
+
+export const findFirstIdentityIcon = (user: UserPrismaType): string | null => {
+  const identities = user.Identities;
+  for (const identity of identities) {
+    if (identity.IdentityBlockchain) {
+      continue;
+    } else if (identity.IdentityEmail && identity.IdentityEmail.icon) {
+      return identity.IdentityEmail.icon;
+    } else if (identity.IdentityDiscord && identity.IdentityDiscord.avatar) {
+      return DiscordApi.createAvatarURL(
+        identity.IdentityDiscord.discordUserId,
+        identity.IdentityDiscord.avatar,
+      );
+    }
+  }
+  return null;
 };
