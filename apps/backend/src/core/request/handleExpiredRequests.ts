27a95a3a4afd0cd0edaacac01b6808df89c702c0
Tyler Sullberg <tysullberg@gmail.com>
2024-05-21 11:47:27 -0700

    Fix script to handle expired request steps.

--- apps/backend/src/core/request/handleExpiredRequests.ts
+++ apps/backend/src/core/request/handleExpiredRequests.ts
@@ -5,13 +5,14 @@ import { runResultsAndActions } from "../result/newResults/runResultsAndActions"
 
 // run results and actions on newly expired requests
 export const handleExpiredResults = async ({}: {}) => {
+  console.log("inside handle expired requests");
   await prisma.$transaction(async (transaction) => {
     const now = new Date();
 
     // get request steps that are past expiration date but haven't been processed yet
     const newlyExpiredSteps = await transaction.requestStep.findMany({
       where: {
-        expired: false,
+        responseComplete: false,
         expirationDate: { lte: now },
       },
       include: {
@@ -24,13 +25,14 @@ export const handleExpiredResults = async ({}: {}) => {
       },
     });
 
+    // stop allowing responses on expired requests
     await transaction.requestStep.updateMany({
       where: {
-        expired: false,
+        responseComplete: false,
         expirationDate: { lte: now },
       },
       data: {
-        expired: true,
+        responseComplete: true,
       },
     });
 
