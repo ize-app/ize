234441899f3f7ab616918396221ff1d121a47f8e
Tyler Sullberg <tysullberg@gmail.com>
2024-05-28 11:51:06 -0700

    Add back sorting imports.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,13 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "prettier", "import"],
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "prettier", // Integrate with Prettier for code formatting
+    "plugin:import/recommended",
+    "plugin:import/typescript",
   ],
   parserOptions: {
     project: "./tsconfig.json",
@@ -18,6 +20,50 @@ module.exports = {
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    "sort-imports": [
+      "error",
+      {
+        ignoreCase: false,
+        ignoreDeclarationSort: true, // don"t want to sort import lines, use eslint-plugin-import instead
+        ignoreMemberSort: false,
+        memberSyntaxSortOrder: ["none", "all", "multiple", "single"],
+        allowSeparatedGroups: true,
+      },
+    ],
+    // turn on errors for missing imports
+    "import/no-unresolved": "error",
+    "import/no-named-as-default-member": "off",
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin", // Built-in imports (come from NodeJS native) go first
+          "external", // <- External imports
+          "internal", // <- Absolute imports
+          ["sibling", "parent"], // <- Relative imports, the sibling and parent types they can be mingled together
+          "index", // <- index imports
+          "unknown", // <- unknown
+        ],
+        "newlines-between": "always",
+        alphabetize: {
+          /* sort in ascending order. Options: ["ignore", "asc", "desc"] */
+          order: "asc",
+          /* ignore case. Options: [true, false] */
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    // use tsconfigs of root project and packages to determine import resolution
+    "import/resolver": {
+      typescript: {
+        project: ["./tsconfig.json", "./apps/**/tsconfig.json", "./apps/**/tsconfig.node.json"],
+      },
+    },
   },
   ignorePatterns: ["node_modules/", "dist/", "build/", ".eslintrc.cjs"],
 };
