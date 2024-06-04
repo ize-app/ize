58c96f7c247493ebc317b3c17aa08946848246bc
Tyler Sullberg <tysullberg@gmail.com>
2024-06-04 12:18:15 -0700

    Split alchemy client into multiple api keys.

--- apps/backend/src/blockchain/alchemyClient/alchemyClient.ts
+++ apps/backend/src/blockchain/alchemyClient/alchemyClient.ts
@@ -1,12 +1,28 @@
 import { Network } from "alchemy-sdk";
 
-import { AlchemyMultichainClient } from "./AlchemyMultichainClient";
+import { AlchemyMultichainClient, AlchemyMultichainSettings } from "./AlchemyMultichainClient";
 
 const defaultConfig = {
-  apiKey: process.env.ALCHEMY_API_KEY as string,
+  apiKey: process.env.ALCHEMY_API_KEY_ETHEREUM as string,
   network: Network.ETH_MAINNET,
 };
 
-const overrides = {};
+const overrides: Partial<Record<Network, AlchemyMultichainSettings>> = {
+  [Network.ETH_MAINNET]: {
+    apiKey: process.env.ALCHEMY_API_KEY_ETHEREUM as string,
+  },
+  [Network.OPT_MAINNET]: {
+    apiKey: process.env.ALCHEMY_API_KEY_OPTIMISM as string,
+  },
+  [Network.BASE_MAINNET]: {
+    apiKey: process.env.ALCHEMY_API_KEY_BASE as string,
+  },
+  [Network.ARB_MAINNET]: {
+    apiKey: process.env.ALCHEMY_API_KEY_ARBITRUM as string,
+  },
+  [Network.MATIC_MAINNET]: {
+    apiKey: process.env.ALCHEMY_API_KEY_POLYGON as string,
+  },
+};
 
 export const alchemyClient = new AlchemyMultichainClient(defaultConfig, overrides);
