a5977d4938e0d0c22cc975352af2ef7fa79c6ef8
Tyler Sullberg <tysullberg@gmail.com>
2024-06-06 14:03:05 -0700

    Fix redirection bug.

--- apps/backend/src/express/origins.ts
+++ apps/backend/src/express/origins.ts
@@ -1,5 +1,5 @@
 const prodRenderUrl = process.env.PROD_RENDER_URL ?? "https://ize.onrender.com";
-const localUrl = process.env.LOCAL_URL ?? "http://localhost:5173";
+export const localUrl = process.env.LOCAL_URL ?? "http://localhost:5173";
 export const prodUrl = process.env.PROD_URL ?? "https://ize.space";
 
 export const validOrigins = [prodUrl, prodRenderUrl, localUrl];
--- apps/backend/src/stytch/redirectAtLogin.ts
+++ apps/backend/src/stytch/redirectAtLogin.ts
@@ -1,19 +1,31 @@
 import { Request, Response } from "express";
 
-import { prodUrl, validOrigins } from "@/express/origins";
+import { localUrl, prodUrl, validOrigins } from "@/express/origins";
 
-// // for oauth/magiclink auth, stytch redirects to `http://${host}${path}?next_route={}`
-// // this handler redirects to whatever url is defined in next_route params
-
+// for oauth/magiclink auth, stytch redirects to `http://${host}${path}?next_route={}`
+// this handler redirects to whatever url is defined in next_route params
 export const redirectAtLogin = ({ req, res }: { req: Request; res: Response }) => {
   const { next_route } = req.query;
   let redirectURL: URL;
+  const isDev = process.env.MODE === "development";
 
+  if (!next_route) {
+    // If next_route is not provided, redirect to a default page
+    return res.redirect(isDev ? localUrl : prodUrl);
+  }
+  const nextRoutePath = decodeURIComponent(next_route.toString());
   // check that origin is valid before redirecting
   const validHosts = validOrigins.map((origin) => new URL(origin).host);
-  const validOrigin = req.headers.host ? validHosts.includes(req.headers.host) : false;
-  if (validOrigin) redirectURL = new URL(next_route?.toString() as string, req.headers.origin);
-  else redirectURL = new URL(next_route?.toString() as string, prodUrl);
+  const requestHost = req.headers.host;
+
+  if (requestHost && validHosts.includes(requestHost)) {
+    const origin = req.headers.origin || `https://${requestHost}`;
+    redirectURL = new URL(nextRoutePath, origin);
+  } else if (isDev) {
+    redirectURL = new URL(nextRoutePath, localUrl);
+  } else {
+    redirectURL = new URL(nextRoutePath, prodUrl);
+  }
 
   // removing potentially malicious url params
   redirectURL.search = "";
