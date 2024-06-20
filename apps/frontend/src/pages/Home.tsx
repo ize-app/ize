db0e72f5311d6fe60ecda23c65d44d757e210284
Tyler Sullberg <tysullberg@gmail.com>
2024-06-20 15:39:03 -0700

    Fix home page styling.

--- apps/frontend/src/pages/Home.tsx
+++ apps/frontend/src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import { useContext, useLayoutEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import izeLogoUrl from "@/assets/ize-logo-circle.svg";
 import { LoginButton } from "@/components/Auth/LoginButton";
 import { IzeLogoBackground } from "@/layout/IzeLogoBackground";
 import { Route } from "@/routers/routes";
@@ -10,27 +12,43 @@ import { Route } from "@/routers/routes";
 import { CurrentUserContext } from "../contexts/current_user_context";
 
 const UnauthenticatedHome = () => {
+  const navigate = useNavigate();
   return (
     <IzeLogoBackground>
       <Box
         sx={(theme) => ({
           display: "flex",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           backgroundColor: "white",
-          padding: "30px",
+          padding: "50px",
           gap: "20px",
           borderRadius: "10px",
+          maxWidth: "400px",
           // border: "#6750A4 1px solid",
           [theme.breakpoints.down("sm")]: {
-            flexDirection: "column",
+            width: "80%",
+            padding: "20px",
           },
         })}
       >
-        <Button variant="contained" color="primary" href="/" sx={{ width: "160px" }}>
-          Join Alpha Waitlist
+        <img src={izeLogoUrl} style={{ width: "200px" }} />
+        <Typography textAlign={"center"}>
+          Evolutionary, cross-tool, <br /> collaborative workflows
+          <br />
+        </Typography>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => {
+            navigate("/about");
+          }}
+          sx={{ width: "160px" }}
+        >
+          Learn more
         </Button>
-        <LoginButton />
+        <LoginButton sx={{ width: "160px" }} />
       </Box>
     </IzeLogoBackground>
   );
