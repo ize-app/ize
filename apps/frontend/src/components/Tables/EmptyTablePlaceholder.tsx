bc9da49a0306540de51c3cf9f6edd8868e80d754
Tyler Sullberg <tysullberg@gmail.com>
2024-06-25 14:20:19 -0700

    Create placeholder text if there are no requests or flows.

---
+++ apps/frontend/src/components/Tables/EmptyTablePlaceholder.tsx
@@ -0,0 +1,29 @@
+import { Box } from "@mui/material";
+
+import { IzeLogoBackground } from "@/layout/IzeLogoBackground";
+
+export const EmptyTablePlaceholder = ({ children }: { children: React.ReactNode }) => (
+  <IzeLogoBackground
+    sx={{
+      maxHeight: "900px",
+      minWidth: "400px",
+      backgroundColor: "hsla(0, 100%, 100%, 0.5)",
+      padding: "60px",
+    }}
+  >
+    <Box
+      sx={{
+        width: "100%",
+        backgroundColor: "hsla(0, 100%, 100%, 0.9)",
+        display: "flex",
+        maxWidth: "600px",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: "36px",
+      }}
+    >
+      {children}
+    </Box>
+  </IzeLogoBackground>
+);
