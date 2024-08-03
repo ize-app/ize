950413f374a7d60497823b576b242ffb50833768
Tyler Sullberg <tysullberg@gmail.com>
2024-08-02 21:14:36 -0700

    Fix ts errors (#74)

--- apps/backend/src/core/action/retryActions.ts
+++ apps/backend/src/core/action/retryActions.ts
@@ -4,31 +4,34 @@ import { stepInclude } from "../flow/flowPrismaTypes";
 import { resultInclude } from "../result/resultPrismaTypes";
 
 export const retryActions = async () => {
-  //  check if there are any request steps that don't have completed actions
-  const stepsWithoutActions = await prisma.requestStep.findMany({
-    where: {
-      resultsComplete: true,
-      actionsComplete: false,
-      final: false,
-    },
-    include: {
-      Step: {
-        include: stepInclude,
+  return await prisma.$transaction(async (transaction) => {
+    //  check if there are any request steps that don't have completed actions
+    const stepsWithoutActions = await transaction.requestStep.findMany({
+      where: {
+        resultsComplete: true,
+        actionsComplete: false,
+        final: false,
       },
-      Results: {
-        include: resultInclude,
+      include: {
+        Step: {
+          include: stepInclude,
+        },
+        Results: {
+          include: resultInclude,
+        },
       },
-    },
-  });
+    });
 
-  // retry incomplete actions
-  await Promise.all(
-    stepsWithoutActions.map(async (reqStep) => {
-      await executeAction({
-        step: reqStep.Step,
-        results: reqStep.Results,
-        requestStepId: reqStep.id,
-      });
-    }),
-  );
+    // retry incomplete actions
+    await Promise.all(
+      stepsWithoutActions.map(async (reqStep) => {
+        await executeAction({
+          step: reqStep.Step,
+          results: reqStep.Results,
+          requestStepId: reqStep.id,
+          transaction,
+        });
+      }),
+    );
+  });
 };
