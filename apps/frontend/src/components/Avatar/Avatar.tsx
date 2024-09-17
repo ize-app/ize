ee08f65ffea8a2eac5284c248f28ec80e2c5435d
Tyler Sullberg <tysullberg@gmail.com>
2024-09-17 16:36:26 -0700

    Change avatar logo.

--- apps/frontend/src/components/Avatar/Avatar.tsx
+++ apps/frontend/src/components/Avatar/Avatar.tsx
@@ -2,6 +2,10 @@ import { SxProps } from "@mui/material";
 import MuiAvatar, { AvatarProps as MuiAvatarProps } from "@mui/material/Avatar";
 import Blockies from "react-blockies";
 
+import discordLogoUrl from "@/assets/discord-logo-blue.svg";
+import izeLogo from "@/assets/ize-logo-circle.svg";
+import nftUrl from "@/assets/nft.svg";
+import telegramLogoUrl from "@/assets/telegram-logo.svg";
 import { EntityFragment, UserSummaryPartsFragment } from "@/graphql/generated/graphql";
 
 import { getAvatarString } from "./getAvatarString";
@@ -11,6 +15,30 @@ export interface AvatarProps extends MuiAvatarProps {
   avatar: EntityFragment | UserSummaryPartsFragment;
   size?: string;
 }
+
+const defaultGroupAvatarUrl = (
+  entity: EntityFragment | UserSummaryPartsFragment,
+): string | undefined => {
+  if (entity.__typename === "Group") {
+    if (entity.organization?.icon) {
+      return entity.organization.icon;
+    }
+    switch (entity.groupType.__typename) {
+      case "GroupTelegramChat":
+        return telegramLogoUrl;
+      case "DiscordRoleGroup":
+        return discordLogoUrl;
+      case "GroupNft":
+        return nftUrl;
+      case "GroupCustom":
+        return izeLogo;
+      default:
+        return undefined;
+    }
+  } else {
+    return undefined;
+  }
+};
 
 export const Avatar = ({ avatar, size, ...props }: AvatarProps) => {
   const { sx } = props ?? {};
@@ -22,10 +50,7 @@ export const Avatar = ({ avatar, size, ...props }: AvatarProps) => {
   };
   return (
     <MuiAvatar
-      src={
-        avatar.icon ??
-        (avatar.__typename === "Group" ? avatar.organization?.icon ?? undefined : undefined)
-      }
+      src={avatar.icon ?? defaultGroupAvatarUrl(avatar)}
       alt={avatar.name}
       {...props}
       sx={{ ...defaultStyles, ...sx }}
