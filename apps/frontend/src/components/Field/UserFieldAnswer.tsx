1e0452dc9c56296764ce5ba5d9515a3e93804195
Tyler Sullberg <tysullberg@gmail.com>
2024-10-01 14:55:58 -0400

    Fix user answer FE bug.

--- apps/frontend/src/components/Field/UserFieldAnswer.tsx
+++ apps/frontend/src/components/Field/UserFieldAnswer.tsx
@@ -16,7 +16,7 @@ export const UserFieldAnswer = ({
   return (
     <Box>
       <Box sx={{ display: "flex", justifyContent: "space-between" }}>
-        <AvatarWithName avatar={userFieldAnswer.user} />
+        <AvatarWithName avatar={userFieldAnswer.creator} />
         <Typography variant="description" marginBottom={"4px"}>
           {createdAt}
         </Typography>
