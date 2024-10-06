f8d89f15f88fc9f91c6ebc81a7fa04249760324a
Tyler Sullberg <tysullberg@gmail.com>
2024-10-05 17:47:30 -0700

    Change text of previously included options.

--- apps/frontend/src/components/ConfigDiagram/PanelContainer.tsx
+++ apps/frontend/src/components/ConfigDiagram/PanelContainer.tsx
@@ -2,12 +2,15 @@ import Box from "@mui/material/Box";
 
 export const PanelContainer = ({ children }: { children: React.ReactNode }) => (
   <Box
-    sx={{
+    sx={(theme) => ({
+      [theme.breakpoints.down("md")]: {
+        width: "100%",
+      },
       height: "100%",
-      width: "100%",
+      width: "50%",
       display: "flex",
       flexDirection: "column",
-    }}
+    })}
   >
     {children}
   </Box>
